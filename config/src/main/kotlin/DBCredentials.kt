183599fca429b1910c60bec23e25c395a4bec713
Luciano Augusto Marchezan De Paula <marchezl@sinbad.iro.umontreal.ca>
2025-07-25 13:49:17 -0400

    changes for ssh run

--- config/src/main/kotlin/DBCredentials.kt
+++ config/src/main/kotlin/DBCredentials.kt
@@ -1,6 +1,6 @@
 class DBCredentials {
     companion object {
-        const val dbUri = "neo4j://localhost:7687"
+        const val dbUri = "bolt://localhost:7687"
         const val username = "neo4j"
         const val password = "12345678"
     }
