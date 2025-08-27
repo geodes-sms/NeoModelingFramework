66fac65b06f8bcd0642f9b6bcc1d5eb24633750b
lucianoMarchezan <lucianomarchezan94@gmail.com>
2025-08-27 13:14:47 -0400

    removing unused files

--- NeoEMFEvaluation/NeoEMFEvaluation/src/main/java/DBCredentials.java
+++
@@ -1,7 +0,0 @@
-public class DBCredentials {
-    public static String dbUri = "bolt://localhost:7687";
-    public static String username = "neo4j";
-    public static String  password = "12345678";
-    public static int port = 7687;
-    public static String host = "bolt://localhost";
-}
