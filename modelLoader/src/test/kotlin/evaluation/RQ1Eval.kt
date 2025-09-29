0ebb8c4455f88d7d083b775c9b974549f0239287
lucianoMarchezan <lucianomarchezan94@gmail.com>
2025-09-29 16:28:25 -0400

    rearraging RQ1

--- modelLoader/src/test/kotlin/evaluation/RQ1Eval.kt
+++ modelLoader/src/test/kotlin/evaluation/RQ1Eval.kt
@@ -2,31 +2,38 @@ package evaluation
 
 import org.eclipse.emf.common.util.URI
 import org.eclipse.emf.ecore.*
-import org.eclipse.emf.ecore.EPackage;
-import org.eclipse.emf.ecore.resource.impl.*
-import org.eclipse.emf.ecore.xmi.impl.*
-import java.io.File
+import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl
+import org.eclipse.emf.ecore.xmi.impl.EcoreResourceFactoryImpl
 import org.junit.jupiter.api.Test
+import java.io.File
+
 /*/
 To run this file, follow the steps on the Readme.md
  */
-// test file to evaluate RQ1   
-
-class RQ1Eval{
+// Test file to evaluate RQ1
+class RQ1Eval {
     @Test
     fun runEcoreAnalysis() {
-        exportEcoreMetricsToCSV("../Evaluation/metamodels", "../Evaluation/results/RQ1/metamodels.csv")
+        exportEcoreMetricsToCSV(
+            "../Evaluation/metamodels",
+            "../Evaluation/results/RQ1/metamodels.csv"
+        )
     }
 }
+
+// ---- Data class for metrics ----
 data class EcoreMetrics(
     val modelName: String,
     val numClasses: Int,
     val numAttributes: Int,
     val numReferences: Int,
     val numContainments: Int,
-    val loc: Int
+    val loc: Int,
+    val maxDIT: Int,
+    val maxHagg: Int
 )
 
+// ---- Load Ecore model ----
 fun loadEcoreModel(filePath: String): Collection<EPackage> {
     val resourceSet = ResourceSetImpl()
     resourceSet.resourceFactoryRegistry.extensionToFactoryMap["ecore"] = EcoreResourceFactoryImpl()
@@ -36,58 +43,107 @@ fun loadEcoreModel(filePath: String): Collection<EPackage> {
     return resource.contents.filterIsInstance<EPackage>()
 }
 
-fun analyzeEcore(filePath: String): EcoreMetrics {
+// ---- DIT (Generalisation / Inheritance) ----
+fun computeDIT(
+    eClass: EClass,
+    visited: MutableSet<EClass> = mutableSetOf(),
+    memo: MutableMap<EClass, Int> = mutableMapOf()
+): Int {
+    memo[eClass]?.let { return it }
+    if (!visited.add(eClass)) return 0 // cycle detected
 
-    fun countClassifiers(pkg: EPackage): Triple<Int, Int, Pair<Int, Int>> {
-        var numClasses = 0
-        var numAttributes = 0
-        var numReferences = 0
-        var numContainments = 0
+    val depth = if (eClass.eSuperTypes.isEmpty()) 1
+    else 1 + eClass.eSuperTypes.maxOf { computeDIT(it, visited, memo) }
 
-        // Count classifiers in this package
-        for (classifier in pkg.eClassifiers) {
-            if (classifier is EClass) {
-                numClasses++
-                numAttributes += classifier.eAttributes.size
-                numReferences += classifier.eReferences.count { !it.isContainment }
-                numContainments += classifier.eReferences.count { it.isContainment }
-            }
+    visited.remove(eClass)
+    memo[eClass] = depth
+    return depth
+}
+
+fun computeMaxDIT(allClasses: Collection<EClass>): Int {
+    val memo = mutableMapOf<EClass, Int>()
+    return allClasses.maxOfOrNull { computeDIT(it, mutableSetOf(), memo) } ?: 0
+}
+
+// ---- HAgg (Aggregation / Containment) ----
+fun buildAggregationMap(allClasses: Collection<EClass>): Map<EClass, List<EClass>> {
+    val map = mutableMapOf<EClass, MutableList<EClass>>()
+    for (cls in allClasses) {
+        for (ref in cls.eReferences.filter { it.isContainment }) {
+            val target = ref.eType as? EClass ?: continue
+            map.computeIfAbsent(cls) { mutableListOf() }.add(target)
         }
-
-        // Recurse into subpackages
-        for (subPkg in pkg.eSubpackages) {
-            val (subClasses, subAttributes, subRefsAndConts) = countClassifiers(subPkg)
-            numClasses += subClasses
-            numAttributes += subAttributes
-            numReferences += subRefsAndConts.first
-            numContainments += subRefsAndConts.second
-        }
-
-        return Triple(numClasses, numAttributes, numReferences to numContainments)
     }
+    return map
+}
 
+fun computeHaggDepth(
+    eClass: EClass,
+    aggMap: Map<EClass, List<EClass>>,
+    visited: MutableSet<EClass> = mutableSetOf(),
+    memo: MutableMap<EClass, Int> = mutableMapOf()
+): Int {
+    memo[eClass]?.let { return it }
+    if (!visited.add(eClass)) return 0 // cycle detected
+
+    val children = aggMap[eClass] ?: emptyList()
+    val depth = if (children.isEmpty()) 1 else 1 + children.maxOf { computeHaggDepth(it, aggMap, visited, memo) }
+
+    visited.remove(eClass)
+    memo[eClass] = depth
+    return depth
+}
+
+fun computeMaxHagg(allClasses: Collection<EClass>): Int {
+    val aggMap = buildAggregationMap(allClasses)
+    val memo = mutableMapOf<EClass, Int>()
+    return allClasses.maxOfOrNull { computeHaggDepth(it, aggMap, mutableSetOf(), memo) } ?: 0
+}
+
+// ---- Main analysis ----
+fun analyzeEcore(filePath: String): EcoreMetrics {
     val packages = loadEcoreModel(filePath)
-    var totalClasses = 0
+
+    val allClasses = mutableListOf<EClass>()
     var totalAttributes = 0
     var totalReferences = 0
     var totalContainments = 0
 
-    for (pkg in packages) {
-        val (cls, attr, refsAndConts) = countClassifiers(pkg)
-        totalClasses += cls
-        totalAttributes += attr
-        totalReferences += refsAndConts.first
-        totalContainments += refsAndConts.second
+    fun collectClasses(pkg: EPackage) {
+        for (classifier in pkg.eClassifiers) {
+            if (classifier is EClass) {
+                allClasses.add(classifier)
+                totalAttributes += classifier.eAttributes.size
+                totalReferences += classifier.eReferences.count { !it.isContainment }
+                totalContainments += classifier.eReferences.count { it.isContainment }
+            }
+        }
+        pkg.eSubpackages.forEach { collectClasses(it) }
     }
+
+    for (pkg in packages) {
+        collectClasses(pkg)
+    }
+
+    val maxDIT = computeMaxDIT(allClasses)
+    val maxHagg = computeMaxHagg(allClasses)
 
     val modelName = filePath.substringAfterLast(File.separator).removeSuffix(".ecore")
     val loc = File(filePath).useLines { it.count() }
 
-    return EcoreMetrics(modelName, totalClasses, totalAttributes, totalReferences, totalContainments, loc)
+    return EcoreMetrics(
+        modelName,
+        allClasses.size,
+        totalAttributes,
+        totalReferences,
+        totalContainments,
+        loc,
+        maxDIT,
+        maxHagg
+    )
 }
 
-
-
+// ---- CSV Export ----
 fun exportEcoreMetricsToCSV(directoryPath: String, outputPath: String) {
     val metamodelsDir = File(directoryPath)
     val ecoreFiles = metamodelsDir.walk()
@@ -96,11 +152,13 @@ fun exportEcoreMetricsToCSV(directoryPath: String, outputPath: String) {
         .toList()
 
     val outputFile = File(outputPath)
-    outputFile.writeText("model,LOC,classes,attributes,refs,containments\n")
+    outputFile.writeText("model,LOC,classes,attributes,refs,containments,maxDIT,maxHagg\n")
     for (model in ecoreFiles) {
         val metrics = analyzeEcore(model)
-        outputFile.appendText("${metrics.modelName},${metrics.loc},${metrics.numClasses},${metrics.numAttributes},${metrics.numReferences},${metrics.numContainments}\n")
-
+        outputFile.appendText(
+            "${metrics.modelName},${metrics.loc},${metrics.numClasses}," +
+                    "${metrics.numAttributes},${metrics.numReferences},${metrics.numContainments}," +
+                    "${metrics.maxDIT},${metrics.maxHagg}\n"
+        )
     }
-
 }
