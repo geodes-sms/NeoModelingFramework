d19980cadf71a7d287d6f0dfcdd4985c9f39085b
lucianoMarchezan <lucianomarchezan94@gmail.com>
2026-04-21 16:13:30 -0400

    prepare for rq3

--- config/src/main/kotlin/ManagerBatchSizes.kt
+++ config/src/main/kotlin/ManagerBatchSizes.kt
@@ -2,7 +2,7 @@ class ManagerBatchSizes {
     companion object {
         const val queryLimit = 5000
         const val creatorNodesBatchSize = 20000
-        const val updaterNodesBatchSize = 20000
+        const val updaterNodesBatchSize = 50000
         const val removerNodesBatchSize = 20000
     }
 }
